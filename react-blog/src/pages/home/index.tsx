f418617e807a248b32d8efb60f1f1e59aa7e24ed
HyeonE <mag30102000@gmail.com>
2024-01-03 06:04:30 +0900

    Implement home page

---
+++ react-blog/src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import Carousel from "components/Carousel";
+import PostList from "components/PostList";
+
+const Home = () => {
+  return (
+    <>
+      <Carousel />
+      <PostList />
+    </>
+  );
+};
+
+export default Home;
---
+++ react-blog/src/pages/posts/index.tsx
@@ -0,0 +1,6 @@
+import PostList from "components/PostList";
+
+const PostPage = () => {
+  return <PostList hasNavigation={false} />;
+};
+export default PostPage;
