fcddc1eb9b5cf9bb220551841d776bd9714ff839
HyeonE <mag30102000@gmail.com>
2024-01-03 03:52:27 +0900

    React-router-dom Default Settings and Operation

--- react-blog/src/App.tsx
+++ react-blog/src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<div>Hi</div>} />
+      <Route path="/hi" element={<div>Bye</div>} />
+      <Route path="/bye" element={<div>Birthday</div>} />
+      <Route path="*" element={<Navigate replace to="/" />} />
+    </Routes>
   );
 }
 
