fecee4ab20357b10bed9fa40baf86344e28627cb
HyeonE <mag30102000@gmail.com>
2024-01-04 22:46:14 +0900

    Implement user information retrieval and logout functionality

--- react-blog/src/components/Profile.tsx
+++ react-blog/src/components/Profile.tsx
@@ -1,20 +1,39 @@
-import { Link } from "react-router-dom";
+import AuthContext from "context/AuthContext";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Profile = () => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const onSignOut = async () => {
+    try {
+      const auth = getAuth(app);
+      await signOut(auth);
+      toast.success("로그아웃 성공");
+      navigate("/login");
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.code);
+    }
+  };
+
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image" />
         <div>
-          <div className="profile__email">test@test.com</div>
-          <div className="profile__name">김유저</div>
+          <div className="profile__email">{user?.email}</div>
+          <div className="profile__name">{user?.displayName || "사용자"}</div>
         </div>
       </div>
-      <Link to="/" className="profile__logout">
+      <div role="presentation" className="profile__logout" onClick={onSignOut}>
         로그아웃
-      </Link>
+      </div>
     </div>
   );
 };
-
 export default Profile;
