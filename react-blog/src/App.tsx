d4e0138a0bb934a9719d24b0b13128d710400108
HyeonE <mag30102000@gmail.com>
2024-01-07 23:35:39 +0900

    Implement dark mode functionality

--- react-blog/src/App.tsx
+++ react-blog/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { app } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
@@ -6,9 +6,11 @@ import Router from "./components/Router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Loader from "components/Loader";
+import ThemeContext from "context/ThemeContext";
 
 function App() {
   const auth = getAuth(app);
+  const context = useContext(ThemeContext);
 
   //auth를 체크하기 전(initialize전)에는 loader를 띄어주는용도
   const [init, setInit] = useState<boolean>(false);
@@ -31,10 +33,10 @@ function App() {
   }, [auth]);
 
   return (
-    <>
+    <div className={context.theme === "light" ? "white" : "dark"}>
       <ToastContainer />
       {init ? <Router isAuthentication={isAuthentication} /> : <Loader />}
-    </>
+    </div>
   );
 }
 export default App;
