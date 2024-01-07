665e7e8e521601e0026176b0d1f99b52ace01c5a
HyeonE <mag30102000@gmail.com>
2024-01-07 23:34:43 +0900

    Implement create and update functionality

--- react-blog/src/components/PostForm.tsx
+++ react-blog/src/components/PostForm.tsx
@@ -1,32 +1,64 @@
 import AuthContext from "context/AuthContext";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "firebaseApp";
-import { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
+import { CATEGORIES, CategoryType, PostProps } from "./PostList";
 
 const PostForm = () => {
   const [title, setTitle] = useState<string>("");
   const [summary, setSummary] = useState<string>("");
   const [content, setContent] = useState<string>("");
+  const [post, setPost] = useState<PostProps | null>(null);
+  const [category, setCategory] = useState<CategoryType>("Frontend");
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
+  const params = useParams();
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
-      // firestore로 데이터 생성
-      await addDoc(collection(db, "posts"), {
-        title: title,
-        summary: summary,
-        content: content,
-        createAt: new Date()?.toLocaleDateString(),
-        email: user?.email,
-      });
+      if (post && post.id) {
+        // post가 있다면 firestore로 데이터 수정
+        const postRef = doc(db, "posts", post?.id);
+        await updateDoc(postRef, {
+          title: title,
+          summary: summary,
+          content: content,
+          email: user?.email,
 
-      toast?.success("게시글을 생성했습니다.");
-      navigate("/");
+          updatedAt: new Date()?.toLocaleDateString("ko", {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+          }),
+        });
+
+        toast?.success("게시글을 수정했습니다.");
+        navigate(`/posts/${post.id}`);
+      } else {
+        // firestore로 데이터 생성
+        await addDoc(collection(db, "posts"), {
+          title: title,
+          summary: summary,
+          content: content,
+          email: user?.email,
+          uid: user?.uid,
+          category: category,
+          createdAt: new Date()?.toLocaleDateString("ko", {
+            /*toLocalDateString으로 넣게 되면 월일만 들어가기 때문에 최신순으로 보여지지 않음
+            추가로 한국 시간 기준으로 시분초를 같이 넣어 최신으로 볼수 있도록 시간 설정*/
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+          }),
+        });
+
+        toast?.success("게시글을 생성했습니다.");
+        navigate("/");
+      }
     } catch (e: any) {
       console.log(e);
       toast?.error(e?.code);
@@ -34,7 +66,9 @@ const PostForm = () => {
   };
 
   const onChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
   ) => {
     const {
       target: { name, value },
@@ -51,7 +85,33 @@ const PostForm = () => {
     if (name === "content") {
       setContent(value);
     }
+    if (name === "category") {
+      setCategory(value as CategoryType);
+    }
   };
+
+  const getPost = async (id: string) => {
+    if (id) {
+      const docRef = doc(db, "posts", id);
+      const docSnap = await getDoc(docRef);
+
+      //docSnap이 데이터를 가지고 있는것이 아니라 데이터를 한번더 호출해야함
+      setPost({ id: docSnap.id, ...(docSnap.data() as PostProps) });
+    }
+  };
+
+  useEffect(() => {
+    //params에 id가 있을때만 상세페이지의 게시물을 가져올수 있기 때문에 id가 있을때 getPost를 함
+    if (params?.id) getPost(params?.id);
+  }, [params?.id]);
+
+  useEffect(() => {
+    if (post) {
+      setTitle(post?.title);
+      setSummary(post?.summary);
+      setContent(post?.content);
+    }
+  }, [post]);
 
   return (
     <form action="/post" method="POST" className="form" onSubmit={onSubmit}>
@@ -64,6 +124,22 @@ const PostForm = () => {
           required
           onChange={onChange}
         />
+      </div>
+      <div className="form__block">
+        <label htmlFor="category">카테고리</label>
+        <select
+          name="category"
+          id="category"
+          onChange={onChange}
+          defaultValue={category}
+        >
+          <option value="">카테고리를 선택해주세요</option>
+          {CATEGORIES?.map((category) => (
+            <option value={category} key={category}>
+              {category}
+            </option>
+          ))}
+        </select>
       </div>
       <div className="form__block">
         <label htmlFor="summary">요약</label>
@@ -80,7 +156,11 @@ const PostForm = () => {
         <textarea name="content" id="content" required onChange={onChange} />
       </div>
       <div className="form__block">
-        <input type="submit" value="제출" className="form__btn--submit" />
+        <input
+          type="submit"
+          value={post ? "수정" : "제출"}
+          className="form__btn--submit"
+        />
       </div>
     </form>
   );
---
+++ react-blog/src/pages/posts/edit.tsx
@@ -0,0 +1,11 @@
+import PostForm from "components/PostForm";
+
+const PostEdit = () => {
+  return (
+    <>
+      <PostForm />
+    </>
+  );
+};
+
+export default PostEdit;
