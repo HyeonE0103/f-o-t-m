bfba411843b3fcf30f2be7cd16e57eee4dec5f3d
HyeonE <mag30102000@gmail.com>
2024-01-07 23:35:18 +0900

    Implement detail view and delete functionality

--- react-blog/src/components/PostDetail.tsx
+++ react-blog/src/components/PostDetail.tsx
@@ -1,13 +1,16 @@
-import { doc, getDoc } from "firebase/firestore";
+import { deleteDoc, doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { PostProps } from "./PostList";
 import { db } from "firebaseApp";
 import Loader from "./Loader";
+import { toast } from "react-toastify";
+import { Comments } from "./Comments";
 
 const PostDetail = () => {
   const [post, setPost] = useState<PostProps | null>(null);
   const params = useParams();
+  const navigate = useNavigate();
 
   const getPost = async (id: string) => {
     if (id) {
@@ -19,8 +22,14 @@ const PostDetail = () => {
     }
   };
 
-  const handleDelete = () => {
-    console.log("delete!");
+  const handleDelete = async () => {
+    //윈도우에 내장되어있는 confirm을 이용하여 유저에게 한번 더 삭제할지 물어봄
+    const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+    if (confirm && post && post.id) {
+      await deleteDoc(doc(db, "posts", post.id));
+      toast?.success("게시글을 삭제했습니다.");
+      navigate("/");
+    }
   };
 
   useEffect(() => {
@@ -32,29 +41,35 @@ const PostDetail = () => {
     <>
       <div className="post__detail">
         {post ? (
-          <div className="post__box">
-            <div className="post__title">{post?.title}</div>
-            <div className="post__profile-box">
-              <div className="post__profile" />
-              <div className="post__author-name">{post?.email}</div>
-              <div className="post__date">{post?.createdAt}</div>
-            </div>
-            <div className="post__utils-box">
-              <div
-                className="post__delete"
-                role="presentation"
-                onClick={handleDelete}
-              >
-                삭제
+          <>
+            <div className="post__box">
+              <div className="post__title">{post?.title}</div>
+              <div className="post__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">{post?.email}</div>
+                <div className="post__date">{post?.createdAt}</div>
               </div>
-              <div className="post__edit">
-                <Link to={`/posts/edit/1`}>수정</Link>
+              <div className="post__utils-box">
+                {post?.category && (
+                  <div className="post__category">{post?.category}</div>
+                )}
+                <div
+                  className="post__delete"
+                  role="presentation"
+                  onClick={handleDelete}
+                >
+                  삭제
+                </div>
+                <div className="post__edit">
+                  <Link to={`/posts/edit/${post?.id}`}>수정</Link>
+                </div>
+              </div>
+              <div className="post__text post__text--pre-wrap">
+                {post?.content}
               </div>
             </div>
-            <div className="post__text post__text--pre-wrap">
-              {post?.content}
-            </div>
-          </div>
+            <Comments post={post} getPost={getPost}/>
+          </>
         ) : (
           <Loader />
         )}
