bcba5ade85c1ca480d2801c21efce6c53e1dcd97
HyeonE <mag30102000@gmail.com>
2024-01-09 23:49:10 +0900

    Implement footer UI, functionality, and logout feature

---
+++ react-twitter/src/components/MenuList.tsx
@@ -0,0 +1,53 @@
+import { BsHouse } from "react-icons/bs";
+import { BiUserCircle } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import { MdLogout, MdLogin } from "react-icons/md";
+import { useContext } from "react";
+import AuthContext from "context/AuthContext";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+
+import { toast } from "react-toastify";
+import { AiOutlineSearch } from "react-icons/ai";
+
+const MenuList = () => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const onLogout = async () => {
+    const auth = getAuth(app);
+    await signOut(auth);
+    toast.success("로그아웃 되었습니다.");
+  };
+
+  return (
+    <div className="footer">
+      <div className="footer__grid">
+        <button type="button" onClick={() => navigate("/")}>
+          <BsHouse />
+          Home
+        </button>
+        <button type="button" onClick={() => navigate("/profile")}>
+          <BiUserCircle />
+          Profile
+        </button>
+        <button type="button" onClick={() => navigate("/search")}>
+          <AiOutlineSearch />
+          Search
+        </button>
+        {user === null ? (
+          <button type="button" onClick={() => navigate("/users/login")}>
+            <MdLogin />
+            Login
+          </button>
+        ) : (
+          <button type="button" onClick={onLogout}>
+            <MdLogout />
+            Logout
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+export default MenuList;
