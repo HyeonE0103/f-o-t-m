dd8bb1004e0cba39388c1d72b55318da668ee2de
HyeonE <mag30102000@gmail.com>
2024-01-09 23:42:00 +0900

    Implement routers based on permissions

---
+++ react-twitter/src/App.tsx
@@ -0,0 +1,41 @@
+import Latyout from "components/Latyout";
+import Router from "components/Router";
+import { app } from "firebaseApp";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "components/loader/Loader";
+
+function App() {
+  const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
+  return (
+    <Latyout>
+      <ToastContainer
+        theme="dark"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop
+      />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
+    </Latyout>
+  );
+}
+
+export default App;
