485abe22f4b3063e0cf45b35db6190ff84eca441
HyeonE <mag30102000@gmail.com>
2024-01-10 00:09:33 +0900

    Create home UI and display all posts

---
+++ react-twitter/src/pages/home/index.tsx
@@ -0,0 +1,65 @@
+import PostForm from "components/posts/PostForm";
+import PostBox from "components/posts/PostBox";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "context/AuthContext";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "firebaseApp";
+
+export interface PostProps {
+  id: string;
+  email: string;
+  content: string;
+  createdAt: string;
+  uid: string;
+  profileUrl?: string;
+  likes?: string[];
+  likeCount?: number;
+  comments?: any;
+  hashTags?: string[];
+  imageUrl?: string;
+}
+
+const HomePage = () => {
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    //게시물 불러오기
+    if (user) {
+      let postsRef = collection(db, "posts"); //posts라는 콜렉션가져오고
+      let postsQuery = query(postsRef, orderBy("createdAt", "desc")); //query를 이용하여 내림차순으로 정렬
+
+      onSnapshot(postsQuery, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [user]);
+
+  return (
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">Home</div>
+        <div className="home__tabs">
+          <div className="home__tab home__tab--active">For You</div>
+          <div className="home__tab">Following</div>
+        </div>
+      </div>
+
+      <PostForm />
+      <div className="post">
+        {posts?.length > 0 ? (
+          posts?.map((post) => <PostBox post={post} key={post.id} />)
+        ) : (
+          <div className="post__no-posts">
+            <div className="post__text">게시글이 없습니다.</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+export default HomePage;
