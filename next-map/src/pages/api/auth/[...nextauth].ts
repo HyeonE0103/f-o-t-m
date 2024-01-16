6c0899e4b6d113376015dbe8a4d9eb31da1dca8a
HyeonE <mag30102000@gmail.com>
2024-01-17 00:50:35 +0900

    Implement user authentication with NextAuth.js

---
+++ next-map/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,40 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+import GoogleProvider from "next-auth/providers/google";
+import NaverProvider from "next-auth/providers/naver";
+import KakaoProvider from "next-auth/providers/kakao";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  session: {
+    //해당 옵션이 세션관리 방법중 하나로 jwt기반의 세션을 사용한다고 명시
+    strategy: "jwt" as const, //기본값인 jwt를 명시, type에러를 해결하기이해 const명시
+    maxAge: 60 * 60 * 24, //세션의 최대수명 설정. 초단위로 설정. 24시간
+    updateAge: 60 * 60 * 2, //세션을 업데이트 하는 주기. 초단위로 설정. 2시간
+  },
+  adapter: PrismaAdapter(prisma),
+  // Configure one or more authentication providers
+  providers: [
+    // ...add more providers here
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID || "",
+      clientSecret: process.env.NAVER_CLIENT_SECRET || "",
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID || "",
+      clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
+    }),
+  ],
+  pages: {
+    signIn: "/users/login",
+  },
+};
+
+export default NextAuth(authOptions);
