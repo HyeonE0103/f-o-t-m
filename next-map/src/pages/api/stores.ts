3817b741e295fd7c8bcfa126266a9fcf844e0754
HyeonE <mag30102000@gmail.com>
2024-01-18 02:00:14 +0900

    Implement post deletion functionality

--- next-map/src/pages/api/stores.ts
+++ next-map/src/pages/api/stores.ts
@@ -1,54 +1,112 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { StoreApiResponse, StoreType } from "@/interface";
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/db";
+import axios from "axios";
 
 interface Responsetype {
   page?: string;
   limit?: string;
   q?: string;
   district?: string;
+  id?: string;
 }
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<StoreApiResponse | StoreType[] | StoreType> //페이지네이션, 전체데이터, 한개의 데이터
+  res: NextApiResponse<StoreApiResponse | StoreType[] | StoreType | null> //페이지네이션, 전체데이터, 한개의 데이터
 ) {
-  const { page = "", limit = "", q, district }: Responsetype = req.query;
-  const prisma = new PrismaClient();
+  const { page = "", limit = "", q, district, id }: Responsetype = req.query;
+  if (req.method === "POST") {
+    //데이터 생성 처리
+    const formData = req.body;
+    const headers = {
+      Authorization: `KakaoAK ${process.env.KAKAO_CLIENT_ID}`,
+    };
 
-  if (page) {
-    //page query가 있는 경우 페이지네이션
-    const count = await prisma.store.count();
-    const skipPage = parseInt(page) - 1;
-    const stores = await prisma.store.findMany({
-      orderBy: { id: "asc" },
-      where: {
-        name: q ? { contains: q } : {},
-        //q값이 있으면 name에 q를 포함하는 게시물을 가져오고 아니면 빈쿼리(아무거나 괜춘)
-        address: district ? { contains: district } : {},
-      },
-      take: parseInt(limit), //한페이지당 10개씩의 게시물을 보여줌
-      skip: skipPage * 10,
-      //skip은 게시물 얼마나 띄어넘어서 보여줄것인지 나타냄
+    //https://dapi.kakao.com/v2/local/search/address.원하는타입?query=인코더된주소
+    const { data } = await axios.get(
+      `https://dapi.kakao.com/v2/local/search/address.json?query=${encodeURI(
+        formData.address
+      )}`,
+      { headers } //헤더값 추가
+    );
+
+    const result = await prisma.store.create({
+      data: { ...formData, lat: data.documents[0].y, lng: data.documents[0].x },
     });
 
-    res.status(200).json({
-      page: parseInt(page), //현재 페이지
-      data: stores, //그에 따른 데이터
-      totalCount: count, //전체 페이지 개수
-      totalPage: Math.ceil(count / 10), //정수로 딱 떨어지지 않기 때문에 올림
+    return res.status(200).json(result);
+  } else if (req.method === "PUT") {
+    // 데이터 수정. POST와 구현이 거의 같지만 prisma를 수정하는 것이 아닌 해당 id값인 데이터를 update함
+    const formData = req.body;
+    const headers = {
+      Authorization: `KakaoAK ${process.env.KAKAO_CLIENT_ID}`,
+    };
+
+    const { data } = await axios.get(
+      `https://dapi.kakao.com/v2/local/search/address.json?query=${encodeURI(
+        formData.address
+      )}`,
+      { headers }
+    );
+
+    const result = await prisma.store.update({
+      where: { id: formData.id }, //where문으로 어떤 데이터를 업데이트할 것인지
+      data: { ...formData, lat: data.documents[0].y, lng: data.documents[0].x },
     });
+
+    return res.status(200).json(result);
+  } else if (req.method === "DELETE") {
+    // 데이터 삭제
+    if (id) {
+      //id가 있을 경우에는
+      const result = await prisma.store.delete({
+        //delete를 이용하여 DB에서 해당 게시물 삭제
+        where: {
+          //where문으로 어떤 데이터를 삭제할 것인지
+          id: parseInt(id), //id는 string으로 되어있긴 때문에 int로 변경
+        },
+      });
+
+      return res.status(200).json(result);
+    }
+    return res.status(500).json(null); //서버에러가 났을 경우 null값을 보냄
   } else {
-    const { id }: { id?: string } = req.query;
+    //GET 요청 처리
+    if (page) {
+      //page query가 있는 경우 페이지네이션
+      const count = await prisma.store.count();
+      const skipPage = parseInt(page) - 1;
+      const stores = await prisma.store.findMany({
+        orderBy: { id: "asc" },
+        where: {
+          name: q ? { contains: q } : {},
+          //q값이 있으면 name에 q를 포함하는 게시물을 가져오고 아니면 빈쿼리(아무거나 괜춘)
+          address: district ? { contains: district } : {},
+        },
+        take: parseInt(limit), //한페이지당 10개씩의 게시물을 보여줌
+        skip: skipPage * 10,
+        //skip은 게시물 얼마나 띄어넘어서 보여줄것인지 나타냄
+      });
 
-    //없는 경우 총 데이터 가져오기
-    const stores = await prisma.store.findMany({
-      orderBy: { id: "asc" }, //오름차순 정리
-      where: {
-        id: id ? parseInt(id) : {}, //id가 있으면 해당 id에 게시물을 가져오고 아니면 where문 무시
-      },
-    });
+      res.status(200).json({
+        page: parseInt(page), //현재 페이지
+        data: stores, //그에 따른 데이터
+        totalCount: count, //전체 페이지 개수
+        totalPage: Math.ceil(count / 10), //정수로 딱 떨어지지 않기 때문에 올림
+      });
+    } else {
+      const { id }: { id?: string } = req.query;
 
-    return res.status(200).json(id ? stores[0] : stores);
+      //없는 경우 총 데이터 가져오기
+      const stores = await prisma.store.findMany({
+        orderBy: { id: "asc" }, //오름차순 정리
+        where: {
+          id: id ? parseInt(id) : {}, //id가 있으면 해당 id에 게시물을 가져오고 아니면 where문 무시
+        },
+      });
+
+      return res.status(200).json(id ? stores[0] : stores);
+    }
   }
 }
--- next-map/src/pages/stores/[id]/index.tsx
+++ next-map/src/pages/stores/[id]/index.tsx
@@ -7,11 +7,14 @@ import { StoreType } from "@/interface";
 import Loader from "@/components/Loader";
 import Map from "@/components/Map";
 import Marker from "@/components/Marker";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { toast } from "react-toastify";
 
 export default function StorePage() {
-  const [map, setMap] = useState(null);
   const router = useRouter();
   const { id } = router.query;
+  const { status } = useSession(); //사용자가 로그인했을때만 접근할 수 있도록함
 
   const fetchStore = async () => {
     const { data } = await axios(`/api/stores?id=${id}`);
@@ -30,6 +33,27 @@ export default function StorePage() {
     즉 창이 2개열려있을 경우 왔다갔다 하면 다시 refetch하는데 default true임으로 false로 변경 */
   });
 
+  const handleDelete = async() => {
+    const confirm = window.confirm("해당 가게를 삭제하시겠습니까?");
+    //삭제하기전 유저에게 한번 더 확인
+
+    if (confirm && store) { //유저가 삭제하겠다고 하고 store가 있는 경우에만
+      try {
+        const result = await axios.delete(`/api/stores?id=${store?.id}`);
+
+        if (result.status === 200) {
+          toast.success("가게를 삭제했습니다.");
+          router.replace("/");
+        } else {
+          toast.error("다시 시도해주세요.");
+        }
+      } catch (e) {
+        console.log(e);
+        toast.error("다시 시도해주세요.");
+      }
+    }
+  };
+
   if (isError) {
     return (
       <div className="w-full h-screen mx-auto pt-[10%] text-red-500 text-center font-semibold">
@@ -45,14 +69,34 @@ export default function StorePage() {
   return (
     <>
       <div className="max-w-5xl mx-auto px-4 py-8">
-        <div className="px-4 sm:px-0">
-          <h3 className="text-base font-semibold leading-7 text-gray-900">
-            {store?.name}
-          </h3>
-          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-            {store?.address}
-          </p>
+        <div className="md:flex justify-between items-center py-4 md:py-0">
+          <div className="px-4 sm:px-0">
+            <h3 className="text-base font-semibold leading-7 text-gray-900">
+              {store?.name}
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+              {store?.address}
+            </p>
+          </div>
+          {status === "authenticated" && ( //로그인 한 사용자 즉 status가 authenticated만
+            <div className="flex items-center gap-3 px-4 py-3">
+              <Link
+                className="hover:bg-gray-300 hover:text-black text-sm bg-blue-500 p-2 rounded-md text-white"
+                href={`/stores/${store?.id}/edit`}
+              >
+                수정
+              </Link>
+              <button
+                type="button"
+                onClick={handleDelete}
+                className="hover:bg-gray-300 hover:text-black text-sm bg-blue-500 p-2 rounded-md text-white"
+              >
+                삭제
+              </button>
+            </div>
+          )}
         </div>
+
         <div className="mt-6 border-t border-gray-100">
           <dl className="divide-y divide-gray-100">
             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -117,8 +161,8 @@ export default function StorePage() {
       {isSuccess && (
         <div className="overflow-hidden w-full mb-20 max-w-5xl mx-auto max-h-[600px]">
           {/* 지도가 웹페이지 전체크기로 되어있기 때문에 내가 원하는대로 맞춰 보여주기 위해 overflow-hidden을 하여 넘치는 부분 안보이게 설정 */}
-          <Map setMap={setMap} lat={store?.lat} lng={store?.lng} zoom={1} />
-          <Marker map={map} store={store} />
+          <Map lat={store?.lat} lng={store?.lng} zoom={1} />
+          <Marker store={store} />
         </div>
       )}
     </>
