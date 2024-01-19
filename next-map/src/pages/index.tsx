92a023e30d243867ed0f42ada5dc68bb3d2fb119
HyeonE <mag30102000@gmail.com>
2024-01-19 22:16:46 +0900

    Switch from getStaticProps to getServerSideProps for dynamic marker rendering

--- next-map/src/pages/index.tsx
+++ next-map/src/pages/index.tsx
@@ -15,12 +15,10 @@ export default function Home({ stores }: { stores: any }) {
   );
 }
 
-//정적페이지 생성을 위한 데이터를 가져오는 사전 렌더링 함수
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
   //fetch대신 axios를 사용했기 때문에 json으로 바꾼 부분은 없앰
   return {
     props: { stores: stores.data }, //axios response에서 data만 추출해서 사용
-    revalidate: 60 * 60, //60분 즉 한시간 마다 업데이트
   };
 }
