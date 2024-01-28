5ae6c9a1a79aa5c839b84cff56c46b7d3230834b
HyeonE <mag30102000@gmail.com>
2024-01-28 23:31:19 +0900

    Extract common logic into a reusable function

--- react-wedding/src/App.tsx
+++ react-wedding/src/App.tsx
@@ -1,11 +1,7 @@
-import { useEffect, useState } from 'react'
-
 import classNames from 'classnames/bind'
 import styles from './App.module.scss'
-
 import Heading from '@components/sections/Heading'
 import Video from '@components/sections/Video'
-import { Wedding } from '@models/wedding'
 import ImageGallery from '@components/sections/ImageGallery'
 import Intro from '@components/sections/Intro'
 import Invitation from '@components/sections/Invitation'
@@ -13,51 +9,20 @@ import Calendar from '@components/sections/Calendar'
 import Map from '@components/sections/Map'
 import Contact from '@components/sections/Contact'
 import Share from '@components/sections/Share'
-import FullScreanMessage from '@shared/FullScreanMessage'
 import AttendCountModal from '@components/AttendCountModal'
+import useWedding from '@hooks/useWedding'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  const [wedding, setWedding] = useState<Wedding | null>(null)
-  const [loading, setLoading] = useState<boolean>(false)
-  const [error, setError] = useState<boolean>(false)
+  const { wedding } = useWedding()
+  /*추상화된 커스텀 훅을 사용해 화면을 그릴것만 생각하면 됨*/
 
-  //1. wedding 데이터 호출
-  useEffect(() => {
-    setLoading(true)
-
-    fetch('http://localhost:8888/wedding')
-      .then((res) => {
-        //fetch는 404같은 에러를 만나면 에러를 reject하지 않음
-        if (res.ok === false) {
-          //따라서 따로 예외 처리가 필요함
-          throw new Error('청첩장 정보를 불러오지 못했습니다')
-          //에러 쓰로우 바로 catch로 이동
-        }
-        return res.json()
-      })
-      .then((data) => {
-        setWedding(data)
-      })
-      .catch((e) => {
-        setError(true)
-      })
-      .finally(() => {
-        //성공했든 실패했든 무조건 실행
-        setLoading(false)
-      })
-  }, [])
-
-  if (loading) {
-    return <FullScreanMessage type="loading" />
-  }
-  if (error) {
-    return <FullScreanMessage type="error" />
-  }
-  if (wedding === null) {
+  if (wedding == null) {
+    //데이터가 오기전 undefined일때
     return null
   }
+
   const {
     date,
     galleryImages,
---
+++ react-wedding/src/hooks/useWedding.ts
@@ -0,0 +1,23 @@
+import { getWedding } from '@/api/wedding'
+import { Wedding } from '@/models/wedding'
+import { useQuery } from 'react-query'
+
+const useWedding = () => {
+  const { data, isLoading, error } = useQuery<Wedding>(
+    ['wedding'],
+    () =>
+      getWedding().then((res) => {
+        if (res.ok === false) {
+          throw new Error('청첩장 정보를 불러오지 못했습니다')
+        }
+        return res.json()
+      }),
+    {
+      //내부에서 suspense 방출
+      suspense: true,
+    },
+  )
+
+  return { wedding: data, loading: isLoading, error }
+}
+export default useWedding
