f58f569ff1d3623eec6b467776b9d38b8c5ca4c0
HyeonE <mag30102000@gmail.com>
2024-01-29 23:23:56 +0900

    Set up routes for different pages in the application

---
+++ react-mcard/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from '@pages/Home'
+import TestPage from './pages/Test'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage}></Route>
+        <Route path="/test" Component={TestPage}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
