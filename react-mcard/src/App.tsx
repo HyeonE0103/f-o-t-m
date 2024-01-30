836e947eb627e0468e8decaf8ddb2a7394feef27
HyeonE <mag30102000@gmail.com>
2024-01-30 23:36:18 +0900

    Add scroll-to-top functionality on route changes for a seamless experience

--- react-mcard/src/App.tsx
+++ react-mcard/src/App.tsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ScrollToTop from '@shared/ScrollToTop'
 import HomePage from '@pages/Home'
-import TestPage from './pages/Test'
+import TestPage from '@pages/Test'
+import Card from '@pages/Card'
+import SigninPage from '@pages/Signin'
+import SignupPage from '@pages/Signup'
+import Navbar from '@shared/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+      <Navbar />
       <Routes>
-        <Route path="/" Component={HomePage}></Route>
-        <Route path="/test" Component={TestPage}></Route>
+        <Route path="/" Component={HomePage} />
+        <Route path="/signin" Component={SigninPage} />
+        <Route path="/signup" Component={SignupPage} />
+        <Route path="/test" Component={TestPage} />
+        <Route path="/card/:id" Component={Card} />
       </Routes>
     </BrowserRouter>
   )
