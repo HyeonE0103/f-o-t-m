df7bce049a174efb488a96361ee6681c238d2da8
HyeonE <mag30102000@gmail.com>
2024-01-31 22:11:33 +0900

    Handle user authentication and display alert on login failure

---
+++ react-mcard/src/pages/Signin.tsx
@@ -0,0 +1,47 @@
+import Form from '@/components/signin/Form'
+import { useAlertContext } from '@/contexts/AlertContext'
+import { FormValues } from '@/models/signin'
+import { auth } from '@/remote/firebase'
+import { FirebaseError } from 'firebase/app'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const SigninPage = () => {
+  const { open } = useAlertContext()
+  const navigate = useNavigate()
+
+  const handleSubmit = useCallback(
+    async (formValues: FormValues) => {
+      const { email, password } = formValues
+
+      try {
+        await signInWithEmailAndPassword(auth, email, password)
+        navigate('/') //로그인 성공시 home으로 이동
+      } catch (e) {
+        // firebase 의 에러
+        if (e instanceof FirebaseError) {
+          if (e.code === 'auth/wrong-password') {
+            open({
+              title: '계정의 정보를 다시 확인해주세요',
+              onButtonClick: () => {},
+            })
+
+            return
+          }
+        }
+
+        // 일반적인 에러
+        open({
+          title: '잠시 후 다시 시도해주세요.',
+          onButtonClick: () => {},
+        })
+      }
+    },
+    [open],
+  )
+
+  return <Form onSubmit={handleSubmit} />
+}
+
+export default SigninPage
