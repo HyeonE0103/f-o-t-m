63803c8369afade1992275cb3ed49cd103d82e58
HyeonE <mag30102000@gmail.com>
2024-01-31 22:22:31 +0900

    Implement logout functionality And Add conditional rendering for authentication-aware buttons

--- react-mcard/src/components/shared/Navbar.tsx
+++ react-mcard/src/components/shared/Navbar.tsx
@@ -1,23 +1,48 @@
 import { colors } from '@/styles/colorPalette'
 import { css } from '@emotion/react'
-import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import Button from './Button'
-import Flex from './Flex'
+
+import { useCallback } from 'react'
+import { signOut } from 'firebase/auth'
+import { auth } from '@/remote/firebase'
+import useUser from '@/hooks/auth/useUser'
+
+import Button from '@shared/Button'
+import Flex from '@shared/Flex'
 
 const Navbar = () => {
   const location = useLocation()
   const showSignButton =
     ['/signup', '/signin'].includes(location.pathname) === false
 
+  const user = useUser()
+
+  const handleLogout = useCallback(() => {
+    signOut(auth)
+  }, [])
+
+  const renderButton = useCallback(() => {
+    if (user != null) {
+      //로그인한 상태
+      return <Button onClick={handleLogout}>로그아웃</Button>
+    }
+
+    if (showSignButton) {
+      //로그인 하지 않은 상태
+      return (
+        <Link to="/signin">
+          <Button>로그인/회원가입</Button>
+        </Link>
+      )
+    }
+
+    return null
+  }, [user, showSignButton, handleLogout])
+
   return (
     <Flex justify="space-between" align="center" css={navbarContainerStyle}>
       <Link to="/">홈</Link>
-      {showSignButton && (
-        <Link to="/signup">
-          <Button>로그인/회원가입</Button>
-        </Link>
-      )}
+      {renderButton()}
     </Flex>
   )
 }
