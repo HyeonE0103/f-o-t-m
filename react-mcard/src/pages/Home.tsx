abc06b9b6cf3b15ad0660fbc2169d1168cf562ab
HyeonE <mag30102000@gmail.com>
2024-02-02 22:05:36 +0900

    Integrate suspense and skeleton UI to address Layout Shift issues

--- react-mcard/src/pages/Home.tsx
+++ react-mcard/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import AdBanners from '@/components/home/AdBanners'
 import CardList from '@/components/home/CardList'
+import ListRow from '@/components/shared/ListRow'
 import Top from '@/components/shared/Top'
+import { Suspense } from 'react'
 
 const HomePage = () => {
   return (
@@ -10,7 +12,13 @@ const HomePage = () => {
         subTitle="회원님을 위한 혜택 좋은 카드를 추천해드려요"
       />
       <AdBanners />
-      <CardList />
+      <Suspense
+        fallback={[...new Array(10)].map((_, idx) => (
+          <ListRow.Skeleton key={idx} />
+        ))}
+      >
+        <CardList />
+      </Suspense>
     </div>
   )
 }
