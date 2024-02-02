11fd970d7536ca72fe2af4895fa3bc171ca75b5c
HyeonE <mag30102000@gmail.com>
2024-02-02 21:51:01 +0900

    Add BundleAnalyzerPlugin to craco.config.js for bundle size analysis

--- react-mcard/craco.config.js
+++ react-mcard/craco.config.js
@@ -1,4 +1,9 @@
 const CracoAlias = require('craco-alias')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+const isProduction = process.env.NODE_ENV === 'production'
+//프로덕션 모드로 빌드가 되고 있니?
 
 module.exports = {
   plugins: [
@@ -19,5 +24,9 @@ module.exports = {
       ],
     ],
     plugins: ['@emotion/babel-plugin'],
+  },
+  webpack: {
+    plugins: isProduction ? [] : [new BundleAnalyzerPlugin()],
+    //isProduction일때는 빈배열을 개발모드일때는 번들을 사용
   },
 }
