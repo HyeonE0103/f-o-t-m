7ebb8ad21d214e322538de64f2331dcc08be5ef2
HyeonE <mag30102000@gmail.com>
2024-02-07 22:43:35 +0900

    Integrate settings menu into the navbar component

--- react-lovetrip/src/components/shared/Navbar.tsx
+++ react-lovetrip/src/components/shared/Navbar.tsx
@@ -6,6 +6,7 @@ import { useCallback } from 'react'
 import Button from '@shared/Button'
 import Flex from '@shared/Flex'
 import useUser from '@/hooks/auth/useUser'
+import Spacing from '@shared/Spacing'
 
 const Navbar = () => {
   const location = useLocation()
@@ -18,18 +19,29 @@ const Navbar = () => {
     if (user != null) {
       //로그인한 상태, 마이페이지로 이동
       return (
-        <Link to="/my">
-          <img
-            src={
-              user.photoURL ??
-              'https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-128.png'
-            }
-            alt="user img"
-            width={40}
-            height={40}
-            style={{ borderRadius: '100%' }}
-          />
-        </Link>
+        <Flex align="center">
+          <Link to="/my">
+            <img
+              src={
+                user.photoURL ??
+                'https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-128.png'
+              }
+              alt="유저의 이미지"
+              width={40}
+              height={40}
+              style={{ borderRadius: '100%' }}
+            />
+          </Link>
+          <Spacing size={4} direction="horizontal" />
+          <Link to="/settings">
+            <img
+              src="https://cdn1.iconfinder.com/data/icons/ionicons-outline-vol-2/512/settings-outline-64.png"
+              width={40}
+              height={40}
+              alt=""
+            />
+          </Link>
+        </Flex>
       )
     }
 
