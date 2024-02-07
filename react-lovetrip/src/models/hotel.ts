c2b7574147f05365a126628afbf6020dd949b7a2
HyeonE <mag30102000@gmail.com>
2024-02-07 22:48:13 +0900

    Add forms property to hotel model

--- react-lovetrip/src/models/hotel.ts
+++ react-lovetrip/src/models/hotel.ts
@@ -17,4 +17,26 @@ export interface Hotel {
     }
   }
   recommendHotels: string[]
+  forms: ReservationForm[]
 }
+
+interface BaseForm {
+  //모든 폼이 공통적으로 가지는 요소
+  id: string
+  label: string
+  required: string
+  helpMessage?: string
+}
+
+interface TextFieldForm extends BaseForm {
+  //BaseForm을 가지고 타입 확장
+  type: 'TEXT_FIELD'
+}
+
+interface SelectFieldForm extends BaseForm {
+  type: 'SELECT'
+  options: Array<{ label: string; value: string }>
+}
+
+export type ReservationForm = TextFieldForm | SelectFieldForm
+//TextFieldForm 나 SelectFieldForm 둘중 하나
