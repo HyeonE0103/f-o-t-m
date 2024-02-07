760d37cb5a0ecf7ec15ce4a32ddcfdeba51d5f8a
HyeonE <mag30102000@gmail.com>
2024-02-07 22:27:52 +0900

    Update navigation logic to incorporate qs library for better parameter management

--- react-lovetrip/src/components/hotel/Rooms.tsx
+++ react-lovetrip/src/components/hotel/Rooms.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import qs from 'qs'
+import { useNavigate } from 'react-router-dom'
 
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 
+import { useAlertContext } from '@/contexts/AlertContext'
+import useUser from '@/hooks/auth/useUser'
+import useRooms from './hook/useRooms'
+
 import Flex from '@shared/Flex'
 import Text from '@shared/Text'
-import useRooms from './hook/useRooms'
-import ListRow from '../shared/ListRow'
-import Spacing from '../shared/Spacing'
-import Tag from '../shared/Tag'
-import Button from '../shared/Button'
+import ListRow from '@shared/ListRow'
+import Spacing from '@shared/Spacing'
+import Tag from '@shared/Tag'
+import Button from '@shared/Button'
 import addDelimiter from '@/utils/addDelimiter'
 
 const Rooms = ({ hotelId }: { hotelId: string }) => {
   const { data } = useRooms({ hotelId })
+  const user = useUser()
+  const { open } = useAlertContext()
+  const navigate = useNavigate()
 
   return (
     <Container>
@@ -29,6 +36,14 @@ const Rooms = ({ hotelId }: { hotelId: string }) => {
         {data?.map((room) => {
           const 마감임박인가 = room.avaliableCount === 1
           const 매진인가 = room.avaliableCount === 0
+
+          const params = qs.stringify(
+            {
+              roomId: room.id,
+              hotelId,
+            },
+            { addQueryPrefix: true },
+          )
 
           return (
             <ListRow
@@ -59,7 +74,25 @@ const Rooms = ({ hotelId }: { hotelId: string }) => {
                 />
               }
               right={
-                <Button size="medium" disabled={매진인가}>
+                <Button
+                  size="medium"
+                  disabled={매진인가}
+                  onClick={() => {
+                    if (user == null) {
+                      // 로그인전
+                      open({
+                        title: '로그인이 필요한 기능입니다',
+                        onButtonClick: () => {
+                          navigate('/signin')
+                        },
+                      })
+
+                      return
+                    }
+
+                    navigate(`/schedule${params}`)
+                  }}
+                >
                   {매진인가 === true ? '매진' : '선택'}
                 </Button>
               }
