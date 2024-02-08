4bc0f1ca6ffffbc13f5504abdca11ee96578bad5
HyeonE <mag30102000@gmail.com>
2024-02-08 23:07:08 +0900

    Implement scrollbar animation optimization using requestAnimationFrame and scaleX

--- react-lovetrip/src/pages/Hotel.tsx
+++ react-lovetrip/src/pages/Hotel.tsx
@@ -1,13 +1,16 @@
-import ActionButtons from '@/components/hotel/ActionButtons'
-import Carousel from '@/components/hotel/Carousel'
-import Contents from '@/components/hotel/Contents'
-import useHotel from '@/components/hotel/hook/useHotel'
-import Map from '@/components/hotel/Map'
-import RecommendHotels from '@/components/hotel/RecommendHotels'
-import Review from '@/components/hotel/Review'
-import Rooms from '@/components/hotel/Rooms'
-import Top from '@/components/shared/Top'
+import ActionButtons from '@components/hotel/ActionButtons'
+import Carousel from '@components/hotel/Carousel'
+import Contents from '@components/hotel/Contents'
+import useHotel from '@components/hotel/hook/useHotel'
+import Map from '@components/hotel/Map'
+import RecommendHotels from '@components/hotel/RecommendHotels'
+import Review from '@components/hotel/Review'
+import Rooms from '@components/hotel/Rooms'
+import ScrollProgressBar from '@shared/ScrollProgressBar'
+import Top from '@components/shared/Top'
 import { useParams } from 'react-router-dom'
+import { css } from '@emotion/react'
+import SEO from '@shared/SEO'
 
 const HotelPage = () => {
   const { id } = useParams() as { id: string }
@@ -23,6 +26,8 @@ const HotelPage = () => {
 
   return (
     <div>
+      <SEO title={name} description={comment} image={images[0]} />
+      <ScrollProgressBar style={scrollProgressBarStyles} />
       <Top title={name} subTitle={comment} />
       <Carousel images={images} />
       <ActionButtons hotel={data} />
@@ -34,5 +39,11 @@ const HotelPage = () => {
     </div>
   )
 }
+
+const scrollProgressBarStyles = css`
+  position: sticky;
+  top: 64px;
+  z-index: 2;
+`
 
 export default HotelPage
