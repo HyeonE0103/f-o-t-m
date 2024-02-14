49205ec4f834550635adeb29ead5c7026b5be904
HyeonE <mag30102000@gmail.com>
2024-02-15 00:18:59 +0900

    Add images domain to next.config.js for using external domain images

--- nextjs-myaccount/next.config.mjs
+++ nextjs-myaccount/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  images: {
+    domains: ['cdn1.iconfinder.com', 'cdn4.iconfinder.com', 'localhost'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
