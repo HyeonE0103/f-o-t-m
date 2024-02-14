98ff3718f7441c5a09e742fb11e47885137187fd
HyeonE <mag30102000@gmail.com>
2024-02-15 00:18:03 +0900

    Created test page for adding DB data

---
+++ nextjs-myaccount/src/pages/test.tsx
@@ -0,0 +1,13 @@
+import CardListAddButton from '@/components/test/CardListAddButton'
+import EventBannerAddButton from '@/components/test/EventBannerAddButton'
+
+function test() {
+  return (
+    <div>
+      <EventBannerAddButton />
+      <CardListAddButton />
+    </div>
+  )
+}
+
+export default test
