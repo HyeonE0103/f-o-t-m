5490f4fc5d9745e1b29a1a81fa75bcc1b526b0ed
HyeonE <mag30102000@gmail.com>
2024-02-15 00:13:13 +0900

    Implemented dynamic import for code splitting

--- nextjs-myaccount/src/pages/index.tsx
+++ nextjs-myaccount/src/pages/index.tsx
@@ -1,21 +1,35 @@
-import Head from 'next/head'
-import styled from '@emotion/styled'
-import { css } from '@emotion/react'
+import dynamic from 'next/dynamic'
+import Account from '@/components/home/Account'
+import { BannerSkeleton } from '@/components/home/EventBanners'
+import { CreditScoreSkeleton } from '@/components/home/CreditScore'
+import Spacing from '@/components/shared/Spacing'
+import { CardListSkeleton } from '@/components/home/CardList'
+
+const EventBanners = dynamic(() => import('@/components/home/EventBanners'), {
+  ssr: false,
+  loading: () => <BannerSkeleton />,
+  //파일을 불러올때부터 들썩이게 하고 싶지 않아서
+})
+
+const CreditScore = dynamic(() => import('@/components/home/CreditScore'), {
+  ssr: false,
+  loading: () => <CreditScoreSkeleton />,
+})
+
+const CardList = dynamic(() => import('@/components/home/CardList'), {
+  ssr: false,
+  loading: () => <CardListSkeleton />,
+})
 
 export default function Home() {
   return (
     <>
-      <Container>
-        <div css={bold}>Hi</div>
-      </Container>
+      <EventBanners />
+      <Account />
+      <Spacing size={8} backgroundColor="gray100" />
+      <CreditScore />
+      <Spacing size={8} backgroundColor="gray100" />
+      <CardList />
     </>
   )
 }
-
-const Container = styled.div`
-  background-color: pink;
-`
-
-const bold = css`
-  font-weight: bold;
-`
