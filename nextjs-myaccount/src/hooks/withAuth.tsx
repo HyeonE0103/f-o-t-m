6629287a7f02e58be15300c0ddacb7d34f0ba2a4
HyeonE <mag30102000@gmail.com>
2024-02-19 00:09:56 +0900

    Integrate withAuth and AuthGuard for dynamic meta tag implementation

--- nextjs-myaccount/src/hooks/withAuth.tsx
+++ nextjs-myaccount/src/hooks/withAuth.tsx
@@ -10,10 +10,16 @@ function withAuth<Props = Record<string, never>>(
     const { data, status } = useSession()
     const router = useRouter()
 
-    if (status !== 'loading' && data == null) {
-      //로딩이 아닌데 세션이 비어있는 로그인하지 않은 상태라면 로그인페이지로
-      router.replace('/auth/signin')
+    if (status === 'loading') {
+      //SEO를 위하여 AuthGuard와 withAuth를 통합
+      //AuthGuard의 useSession이 여러단계에 걸쳐서 인증을 체크하기 때문에
+      //첫 상태는 loading으로 판단되어 원하는 카드 컴포넌트를 전달해주는 것이 아닌
+      //null을 그려버려 내가 원하는 SEO가 아닌 기본 SEO에 대한 정보만 있음
+      return null
+    }
 
+    if (data == null) {
+      router.replace('/auth/signin')
       return null
     }
 
