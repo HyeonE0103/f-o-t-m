81a61f3b1bd3b62fc00fbea1e0f22bbd77058c96
HyeonE <mag30102000@gmail.com>
2024-02-19 00:11:45 +0900

    Utilize useReportWebVitals from next/web-vitals for performance monitoring

--- nextjs-myaccount/src/pages/_app.tsx
+++ nextjs-myaccount/src/pages/_app.tsx
@@ -4,9 +4,10 @@ import globalSteyls from '@styles/globalStyles'
 import Layout from '@/components/shared/Layout'
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 import { SessionProvider } from 'next-auth/react'
-import AuthGuard from '@/components/auth/AuthGuard'
 import Navbar from '@shared/Navbar'
 import { AlertContextProvider } from '@/contexts/AlertContext'
+import ErrorBoundary from '@shared/ErrorBoundary'
+import { useReportWebVitals } from 'next/web-vitals'
 
 const client = new QueryClient()
 
@@ -16,6 +17,10 @@ export default function App({
   Component,
   pageProps: { dehydratedState, session, ...pageProps },
 }: AppProps) {
+  useReportWebVitals((metric) => {
+    console.log(metric)
+  })
+
   return (
     <>
       <Layout>
@@ -26,12 +31,12 @@ export default function App({
               {/* 서버측에서 수행한 값을 클라이언트에서 복원해서 사용할수 있기 때문에 이미
             한번 액션 호출이 일어났고 키값으로 저장이 되어있어서 클라이언트 측에서는
             값은 키값으로 접근을 하게 되면은 데이터가 바로 채워진 상태로 사용할 수 있음 */}
-              <AlertContextProvider>
-                <AuthGuard>
+              <ErrorBoundary>
+                <AlertContextProvider>
                   <Navbar />
                   <Component {...pageProps} />
-                </AuthGuard>
-              </AlertContextProvider>
+                </AlertContextProvider>
+              </ErrorBoundary>
             </Hydrate>
           </QueryClientProvider>
         </SessionProvider>
